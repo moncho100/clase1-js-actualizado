71837fcf462f9bb86ae4e2192fbd667b673f291f
Martin Zuvilivia <88513813+moncho100@users.noreply.github.com>
2022-06-27 18:49:55 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,34 @@
+// Ejemplo 1
+let nombre = prompt("Cual es su nombre?")
+console.log("Hola "+ nombre)
+
+
+// Ejemplos de Suma - Resta - Multiplicacion - Division  
+
+// Ejemplo Suma
+let x = 10
+let numero = parseInt(prompt("Ingrese un numero"))
+let resultadoSuma = x + numero
+alert("El resultado de su numero mas 10 es: " + resultadoSuma);
+
+//Ejemplo Resta
+let numero2 = parseInt(prompt("Ingrese un numero"))
+let resultadoResta = numero2 - x
+alert("El resultado de su numero menos 10 es: " + resultadoResta);
+
+//Ejemplo Multiplicacion
+let numero3 = parseInt(prompt("Ingrese un numero"))
+let resultadoMultiplicacion = numero3 * x
+alert("El resultado de su numero por 10 es: " + resultadoMultiplicacion);
+
+//Ejemplo Division
+let numero4 = parseInt(prompt("Ingrese su numero"))
+let resultadoDivision = numero4 / x
+alert("El resultado de su numero dividido 10 es: " + resultadoDivision);
+
+//Ejemplo Concatenacion
+
+let nombre2 = prompt("Ingrese su nombre")
+let apellido = prompt("Ingrese su apellido")
+alert("Bienvenido : " + nombre2 + " " + apellido);
+
